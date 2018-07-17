e438d216c604e8be0419d9d18c7aa05ea6c07d6b
Jordan Gorla <itsJordanDoes@gmail.com>
2018-07-16 22:42:33 -0700

    Add NotesView, ProjectView and ViewView components.

---
+++ src/renderer/components/NotesView.vue
@@ -0,0 +1,72 @@
+<template>
+  <div :class="['notes-view',$store.state.data.settings.notes_view?'open':'closed']">
+
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    name: 'project-view',
+    components: { },
+    methods: {
+      addProject () {
+        this.$router.push({path: '/create-project'})
+      },
+      viewProject (i) {
+        console.log(i)
+        // this.$router.push({path: `/project/${i}/home`, params: { i }})
+      }
+    },
+    computed: mapGetters({
+      projects: 'projects'
+    }),
+    mounted () {
+    },
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "../sass/settings.scss";
+.closed {
+  width:0%;
+  min-width:0% !important;
+}
+.open {
+  width:100%;
+}
+.notes-view{
+  transition: min-width 0.4s ease,width 0.4s ease;
+  min-width:33%;
+  height:100%;
+  background-color:red;
+  .project-container {
+    display:flex;
+    flex-direction: column;
+    .project {
+      padding: 0.75em;
+      cursor: pointer;
+    }
+    .project:hover {
+      background-color:rgba(0,0,0,0.05);
+    }
+  }
+  hr {
+    border-color:rgba(0,0,0,0.05);
+    margin:0;
+    padding:0;
+  }
+  .new-project {
+    padding: 0.75em;
+    cursor:pointer;
+    background-color:rgba(0,0,0,0.05);
+  }
+  .new-project:hover {
+    background-color:rgba(0,0,0,0.1);
+  }
+}
+</style>
---
+++ src/renderer/components/ProjectViewOld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="project-view">
+    <label for="">{{ projects[this.$route.params.id].label }}</label>
+    <content-editor></content-editor>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  import ContentEditor from './ContentEditor'
+  export default {
+    name: 'project-view',
+    components: { ContentEditor },
+    methods: {
+
+    },
+    computed: mapGetters({
+      projects: 'projects'
+    }),
+    mounted () {
+    },
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+@import "../sass/settings.scss";
+.project-view {
+  height:100%;
+  width:100%;
+}
+</style>
